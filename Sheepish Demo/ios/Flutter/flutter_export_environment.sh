2468789a158d1436c60aa8a4481415308c14940b
jerbm527 <49074646+jerbm527@users.noreply.github.com>
2019-11-03 17:41:26 -0600

    Add files via upload

---
+++ Sheepish Demo/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=F:\Sheepish\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\jerbm_000\Desktop\flutter-chat-demo-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=F:\Sheepish\flutter\bin\cache\artifacts\engine\ios"
